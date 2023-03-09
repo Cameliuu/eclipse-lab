6ad1798fa759d0e19356bc1acef7085b26e37efd
Cameliuu <calin.iacob@student.upt.ro>
2023-03-09 16:54:37 +0200

    CHanged printed message once again

--- exercitiiGIT/src/exercitiiGIT/Main.java
+++ exercitiiGIT/src/exercitiiGIT/Main.java
@@ -3,6 +3,9 @@ package exercitiiGIT;
 public class Main {
 	public static void main(String args[])
 	{
-		System.out.println("AL treilea commit");
+		System.out.println("Alex");
+		System.out.println("Este");
+		System.out.println("UN");
+		System.out.println("");
 	}
 }
